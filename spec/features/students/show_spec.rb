0b991890e5a5f141cb31697ca2dd43b6464e29f8
David Gomez <davdg25@gmail.com>
2019-12-06 10:18:21 -0700

    fixed many to many relationship

--- spec/features/students/show_spec.rb
+++ spec/features/students/show_spec.rb
@@ -5,26 +5,31 @@ RSpec.describe 'As A Visitor', type: :feature do
     @david = Student.create!(name: 'David', age: 26, house: "Ryan's House")
     @scott = Student.create!(name: 'Scott', age: 27, house: "Also Ryan's House")
 
-    @module_1_d = @david.courses.create!(name: 'Module 1')
-    @module_2_d = @david.courses.create!(name: 'Module 2')
+    @module_1 = Course.create!(name: 'Module 1')
+    @module_2 = Course.create!(name: 'Module 2')
+    @module_3 = Course.create!(name: 'Module 3')
+    @module_4 = Course.create!(name: 'Module 4')
 
-    @module_1_s = @scott.courses.create!(name: 'Module 1')
-    @module_2_s = @scott.courses.create!(name: 'Module 2')
-    @module_3_s = @scott.courses.create!(name: 'Module 3')
-    @module_4_s = @scott.courses.create!(name: 'Module 4')
+    @module_1_d = StudentCourse.create!(course_id: @module_1.id, student_id: @david.id )
+    @module_2_d = StudentCourse.create!(course_id: @module_2.id, student_id: @david.id )
+
+    @module_1_s = StudentCourse.create!(course_id: @module_1.id, student_id: @scott.id )
+    @module_2_s = StudentCourse.create!(course_id: @module_2.id, student_id: @scott.id )
+    @module_1_s = StudentCourse.create!(course_id: @module_3.id, student_id: @scott.id )
+    @module_2_s = StudentCourse.create!(course_id: @module_4.id, student_id: @scott.id )
   end
 
   it 'When i visit student show page, /students/:student_id I see a list of all their courses' do
     visit "/students/#{@david.id}"
 
-    expect(page).to have_content(@module_1_d.name)
-    expect(page).to have_content(@module_2_d.name)
+    expect(page).to have_content(@module_1.name)
+    expect(page).to have_content(@module_2.name)
 
     visit "/students/#{@scott.id}"
 
-    expect(page).to have_content(@module_1_s.name)
-    expect(page).to have_content(@module_2_s.name)
-    expect(page).to have_content(@module_3_s.name)
-    expect(page).to have_content(@module_4_s.name)
+    expect(page).to have_content(@module_1.name)
+    expect(page).to have_content(@module_2.name)
+    expect(page).to have_content(@module_3.name)
+    expect(page).to have_content(@module_4.name)
   end
 end
