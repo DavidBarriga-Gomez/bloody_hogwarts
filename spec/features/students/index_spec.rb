03e14d7c0be0ebad2416c2769973387cc4bad4a4
David Gomez <davdg25@gmail.com>
2019-12-06 09:36:41 -0700

    Created students index page with name, age, and house

---
+++ spec/features/students/index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor', type: :feature do
+  before(:each) do
+    @david = Student.create!(name: 'David', age: 26, house: "Ryan's House")
+    @scott = Student.create!(name: 'Scott', age: 27, house: "Also Ryan's House")
+  end
+
+  it 'When i visit student index page, /students I see all students with name, age, house' do
+    visit '/students'
+
+      expect(page).to have_content(@david.name)
+      expect(page).to have_content(@david.age)
+      expect(page).to have_content(@david.house)
+
+      expect(page).to have_content(@scott.name)
+      expect(page).to have_content(@scott.age)
+      expect(page).to have_content(@scott.house)
+      save_and_open_page
+  end
+end
